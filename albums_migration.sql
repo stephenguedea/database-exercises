cba81897665616d57c03ad4dd4ece30a52eceabb
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-30 11:24:11 -0600

    Updated file migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100) NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  release_data INT(4) UNSIGNED NOT NULL,
-  sales FLOAT NOT NULL,
-  genre VARCHAR(100) NOT NULL,
+  artist VARCHAR(100),
+  name VARCHAR(50) NOT NULL,
+  release_date INT,
+  sales FLOAT,
+  genre VARCHAR(240),
   PRIMARY KEY (id)
 );
