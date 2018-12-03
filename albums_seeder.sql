0c69fb70e6a3bf83aae96fe0aec0d7bd5fcdb3d2
Stephen Guedea <guedea.stephen@gmail.com>
2018-12-03 12:27:57 -0600

    ALBUM SEEDER

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -52,7 +52,6 @@ VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop / Rock / R&B'),
 
 
 
-
 -- INSERT INTO albums (artist, name, release_date, sales, genre)
 -- VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock');
 -- INSERT INTO albums (artist, name, release_date, sales, genre)
