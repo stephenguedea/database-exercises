d288cdaa0e2d45c636782dea6936f0c99f9cd4dc
Stephen Guedea <guedea.stephen@gmail.com>
2018-12-04 09:23:13 -0600

    MySQL Aliases

---
+++ aliases_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+show tables;
+
+SELECT CONCAT(last_name, ', ', first_name) FROM employees
+LIMIT 10;
+
+SELECT CONCAT(last_name, ', ', first_name), birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
